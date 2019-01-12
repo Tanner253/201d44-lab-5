715d02f24bda13e47fabefe3c369a15a410fc854
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-01-11 16:07:40 -0800

    finished q4

--- app.js
+++ app.js
@@ -78,12 +78,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+var sum4 = sum(sumArr[0], sum(sumArr[1], sumArr[2])[0] )[0];
 
+var message = sumArr + ' was passed in as an array of numbers, and ' + sum4 + ' is their sum.'; 
+return[sum4, message ];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
