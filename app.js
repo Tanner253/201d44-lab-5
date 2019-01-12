de34dd914377d3f69089015cabeac3262cba6b62
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-01-12 15:01:23 -0800

    made some new tries to q6, commit pushing for Rocket

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 'use strict';
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
@@ -131,16 +132,24 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-// var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line//eslint-disable-line
+var array = testDynamicArray;
+// eslint-disable-next-line no-unused-vars
+var ans = 1;
+// eslint-disable-next-line no-unused-vars
+function multiplyAnyArray(dynamicArray) {
+  var i ;
 
-// function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-//   var numbers = multiply(testDynamicArray[0], testDynamicArray[1], multiply( testDynamicArray[2], testDynamicArray[2], multiply( testDynamicArray[3], testDynamicArray[4])))[0];
-//   var message = 'The numbers ' + testDynamicArray + ' have a product of ' + numbers + '.' ;
-//   return [numbers, message] ;
-// }
+  // eslint-disable-next-line no-redeclare
+  for(var i = 0 ; i < array.length ; i ++){
+    ans = multiply(ans, dynamicArray[i])[0];
+    var message = 'The numbers ' + array + ' have a product of ' + ans + '.' ;
+    return [ans, message] ;
+  }
 
+}
 // // Here is the test for multiplyArray(); uncomment it to run it
 // // eslint-disable-next-line no-undef
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas
